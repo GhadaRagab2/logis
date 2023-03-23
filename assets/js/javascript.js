ce9685dfc760c945c1bd642f98a2be633fae8503
GhadaRagab2 <engghadaragab32@gmail.com>
2023-03-23 13:50:05 +0200

    add video

---
+++ assets/js/javascript.js
@@ -0,0 +1,13 @@
+
+
+
+
+// function sendMail() {
+//     var link = "mailto:engghadaragab32@gmail.com"
+//     + "?cc=myCCaddress@example.com"
+//     + "&subject=" + escape("This is my subject")
+//     + "&body=" + escape(document.getElementById('form').value)
+// ;
+
+// window.location.href = link;
+// }
